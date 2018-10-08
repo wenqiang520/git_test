a381e5d8744f7a0b7a68fd11960e7b59e73fde2d
wenqiang <1914301554@qq.com>
2018-10-08 12:50:53 +0800

    把本地工程添加到git

--- git_test/src/com/itheim/Gittest.java
+++ git_test/src/com/itheim/Gittest.java
@@ -4,4 +4,9 @@ public class Gittest {
     public static void main(String[] args) {
         System.out.println("hello gittest");
     }
+
+    public void test(){
+        int a=10;
+        int b=20;
+    }
 }
